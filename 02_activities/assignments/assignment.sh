a5471b245dce5bc8346f1c45b6479993196d2349
Chetan Kumar <49631168+Chetank190@users.noreply.github.com>
2024-08-21 20:49:26 -0400

    final changes made

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -8,7 +8,6 @@ set -x
 # > Add your code below each comment to complete the tasks
 
 # 1. Create 5 directories named dir1, dir2, dir3, dir4, and dir5
-
 mkdir dir1 dir2 dir3 dir4 dir5
 
 # 2. List the contents of the parent directory to verify the presence of the 5 directories
@@ -24,11 +23,9 @@ touch dir2/file1 dir2/file2 dir2/file3 dir2/file4 dir2/file5
 cat dir2/file3
 
 # 6. Delete file4
-
 rm dir2/file4
 
 # 7. Delete directories dir4 and dir5 including all their contents (if any)
-
 rm -r dir4 dir5
 
 # 8. List the contents of the parent directory to verify the deletion of dir4 and dir5
